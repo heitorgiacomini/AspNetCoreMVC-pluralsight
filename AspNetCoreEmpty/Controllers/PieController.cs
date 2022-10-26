e75c4c99eee1b42840aff04ee9eee95a81984d29
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-26 12:13:08 -0300

    Lesson 44. Demo -  Adding Navigation

--- AspNetCoreEmpty/Controllers/PieController.cs
+++ AspNetCoreEmpty/Controllers/PieController.cs
@@ -26,5 +26,17 @@ namespace AspNetCoreEmpty.Controllers
             piesListViewModel.Pies = _pieRepository.Pies;
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
+
+
     }
 }
