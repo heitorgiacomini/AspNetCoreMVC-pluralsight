8577b205c10eff24822fee202d88fcb1ded22740
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-28 15:38:34 -0300

    Lesson 55. Demo -  Creating the Email Tag Helper

---
+++ AspNetCoreEmpty/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreEmpty.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: ContactController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
