62e7d88d26f64ad534b8d46bf9c0a15306e0c917
Heitor Giacomini <heithor_thebest@hotmail.com>
2022-10-29 16:58:22 -0300

    Lesson 61. Demo -  Using Model Binding

--- AspNetCoreEmpty/Controllers/OrderController.cs
+++ AspNetCoreEmpty/Controllers/OrderController.cs
@@ -7,19 +7,47 @@ namespace AspNetCoreEmpty.Controllers
     {
         private readonly AppDbContext _appDbContext;
         private readonly ShoppingCart _shoppingCart;
+        private readonly IOrderRepository _orderRepository;
 
         public OrderController(
             AppDbContext appDbContext,
-            ShoppingCart shoppingCart
+            ShoppingCart shoppingCart,
+            IOrderRepository orderRepository
         )
         {
             _appDbContext = appDbContext;
             _shoppingCart = shoppingCart;
+            _orderRepository = orderRepository;
         }
 
 
         public IActionResult Checkout()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItem();
+            _shoppingCart.ShoppingCartItems = items;
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy...";
             return View();
         }
     }
